5e0f99f9a16b1b4758ee7c8238ca9362d4c4cc5b
hymnis <hymnis@plazed.net>
2020-11-14 16:27:23 +0100

    Updates FW to v0.1.1
    - Updates version number.
    - Updates comments.
    - Updates functions with constrains for input values.
    - Updates LED state setting functions.
    - Updates readme file.
    - Adds mode setting (solid or blink) from serial command.
    - Adds mode handling (act on LEDs).
    - Adds color fade when waiting for connection to host.
    - Adds Travis-CI build support.

--- README.md
+++ README.md
@@ -14,9 +14,70 @@ Since there are both hardware (current) and software (RAM) limitations to how ma
 
 It's possible to adjust the number of LED's used by editing the `NUMPIXELS` variable. Keep in mind how much RAM the Arduino model used has available. With the Neopixel library that this project uses, each LED needs 3 bytes of RAM.
 
+### Dependencies
+
+This FW requires the Adafruit NeoPixel library. The current version is tested with version 1.7.0.
+
+### Serial communication
+
+The serial interface allows for commands and data to be sent in both directions. The message structure is the same for both incoming and outgoing messages but some commands are exclusive for the host or device. For example; only the host will send a set LED message.
+
+#### Connection settings
+
+- Baudrate: 38400
+- Byte size: 8
+- Parity: None
+- Stopbits: 1
+- Timeout: None
+- XonXoff: 0
+- RtsCts: 0
+
+#### Message types
+
+Messages are split into two types: `+` or `-`.
+
+This is always the first character sent.
+
+**+ messages**
+
+`+` is an action message, For example where the host wants to change a state (perform an action) on the device.
+
+The following actions are supported:
+
+- `l`: set LED color  
+  Message example: `+l000111222#`
+- `b`: set brightness  
+  Message example: `+b000#`
+- `m`: set display mode (solid or blinking)  
+  Message example: `+m000#`
+
+**- messages**
+
+`-` is a request message. For example where the host wants information from the device.
+
+The following requests are supported:
+
+- `!`: full information request  
+  Message example: `-!#`
+
+#### Status commands
+
+Status commands are used by both host and device. They can be sent as a singular command or as a terminator to data sequence.
+
+Both host and device can send ping command and then expects the other part to reply with a pong command. If no other command is sent after a minute, a ping is sent to verify a working connection. Until communication is restored the LED(s) will slowly fade.
+
+- `+!`: OK/Pong
+- `-!`: Resend/Request data
+- `+?`: Unknown/Error
+- `-?`: Ping
+
+#### Termination character
+
+All commands and sequences are terminated with a `#`.
+
+This always the last character sent.
 
 
 ## Computer software
 
 The software, daemon/server and client is available at [hymnis/dsul-python](https://github.com/hymnis/dsul-python).
-
--- nano/nano.ino
+++ nano/nano.ino
@@ -3,7 +3,7 @@
 
 #define MAJOR       0
 #define MINOR       1
-#define PATCH       0
+#define PATCH       1
 
 #define NEOPIN      3
 #define NUMPIXELS   1  // NUMPIXELS * 3 = bytes of RAM used
@@ -13,20 +13,120 @@
 Adafruit_NeoPixel *leds;
 int NEOFORMAT = NEO_GRB + NEO_KHZ800;
 uint16_t show_brightness = 50;  // default brightness (1-255)
-uint16_t r, g, b = 0;  // default color, 0 = black/off
+uint16_t r = 0;
+uint16_t g = 64;
+uint16_t b = 0;
+uint32_t current_color = 0;
 uint8_t show_mode = 1; // default mode, 1 = solid
 uint8_t input_count = 0;
 bool input_state = LOW;
 char input_string[10] = "";
-int counter = 0;
+uint8_t time = 0;
+bool wait_state = false;
+uint16_t host_timeout = 60; // default timeout, 60 seconds
 
+
+// set color for given LED
+void setLed(byte index, uint32_t color) {
+  index = index % leds->numPixels();
+  leds->setPixelColor(index, color);
+}
 
 // perform actual LED state update
-void colorWipe(uint32_t c) {
-  for(uint16_t i=0; i<leds->numPixels(); i++) {
-    leds->setPixelColor(i, c);
+void colorWipe(int index, uint32_t color) {
+  if (index == 0) {
+    // set color for all LEDs
+    for(uint16_t i=0; i<leds->numPixels(); i++) {
+      setLed(i, color);
+    }
+  } else {
+    // set color for one LED
+    setLed(index - 1, color);
   }
   leds->show();
+}
+
+// fade through the colors
+void colorFade() {
+  if (r == 64) {
+    r = 63;
+    g = 1;
+    b = 0;
+  } else if (r > 0 && g < 64 && b == 0) {
+    r--;
+    g++;
+  } else if (g == 64) {
+    g = 63;
+    b = 1;
+    r = 0;
+  } else if (g > 0 && b < 64 && r == 0) {
+    g--;
+    b++;
+  } else if (b == 64) {
+    b = 63;
+    r = 1;
+    g = 0;
+  } else if (b > 0 && r < 64 && g == 0) {
+    b--;
+    r++;
+  }
+  colorWipe(0, leds->Color(r, g, b));
+
+  delay(85);
+}
+
+// send OK command
+void sendOk() {
+  Serial.println("+!#");
+  wait_state = false;
+  time = 0;
+}
+
+// send NOK command
+void sendNOK() {
+  Serial.println("-!#");
+}
+
+// send a ping command
+void sendPing() {
+  Serial.println("-?#");
+}
+
+// reset input variables
+void resetInput() {
+  input_state = LOW;
+  input_count = 0;
+  memset(input_string, 0, sizeof input_string);
+}
+
+// receive and store incomming serial data
+bool receiveInput() {
+  bool new_state = LOW;
+  int input = Serial.read();
+
+  if (input_state == HIGH) {
+    if (input == 35) {  // #
+      // sequence complete
+      new_state = HIGH;
+    } else {
+      // receiving data
+      if (input_count > 10) {
+        input_state = LOW;
+        sendNOK();
+        resetInput();
+      } else {
+        input_string[input_count] = input;
+        input_count += 1;
+      }
+    }
+  } else if ((input == 43 || input == 45) && input_state == LOW) {  // + or -
+    // receiving command/request
+    input_state = HIGH;
+    input_string[input_count] = input;
+    input_count += 1;
+  }
+
+  return new_state;
 }
 
 // handle the input sequence
@@ -37,24 +137,27 @@ void handleInput() {
   if (type == '-') {  // - = 45
     if (key == '!') {  // ! = 33
       // information requested
-      // respond with color, brightness, mode and version
+      // respond with version, color, brightness and mode
+      char response_v[10];
       char response_c[10];
       char response_b[4];
       char response_m[4];
-      char response_v[10];
 
+      sprintf(response_v, "v%03d.%03d.%03d", MAJOR, MINOR, PATCH);
+      Serial.print(response_v);
       sprintf(response_c, "c%03d%03d%03d", r, g, b);
       Serial.print(response_c);
       sprintf(response_b, "b%03d", show_brightness);
       Serial.print(response_b);
       sprintf(response_m, "m%03d", show_mode);
       Serial.print(response_m);
-      sprintf(response_v, "v%03d.%03d.%03d", MAJOR, MINOR, PATCH);
-      Serial.print(response_v);
-      Serial.print("+!#");
+
+      sendOk();
     } else if (key == '?') {  // ? = 63
       // ping received
-      Serial.print("+!#");
+      sendOk();
+    } else {
+      sendNOK(); // unknown key, not OK
     }
   } else if (type == '+') {  // + = 43
     if (key == 'l') {
@@ -64,18 +167,25 @@ void handleInput() {
       color_value[0] = input_string[2];
       color_value[1] = input_string[3];
       color_value[2] = input_string[4];
+      color_value[3] = '\0';
       r = atoi(color_value);
+      r = constrain(r, 0, 255);
       color_value[0] = input_string[5];
       color_value[1] = input_string[6];
       color_value[2] = input_string[7];
+      color_value[3] = '\0';
       g = atoi(color_value);
+      g = constrain(g, 0, 255);
       color_value[0] = input_string[8];
       color_value[1] = input_string[9];
       color_value[2] = input_string[10];
+      color_value[3] = '\0';
       b = atoi(color_value);
-  
-      colorWipe(leds->Color(r, g, b));
-      Serial.print("+!#");
+      b = constrain(b, 0, 255);
+
+      current_color = leds->Color(r, g, b);
+      colorWipe(0, current_color);
+      sendOk();
     } else if (key == 'b') {
       // set led BRIGHTNESS
       // b xxx
@@ -83,79 +193,94 @@ void handleInput() {
       bright_value[0] = input_string[2];
       bright_value[1] = input_string[3];
       bright_value[2] = input_string[4];
+      bright_value[3] = '\0';
       show_brightness = atoi(bright_value);
-  
-      if (show_brightness < BRIGHT_MIN)
-        show_brightness = BRIGHT_MIN;
-      if (show_brightness > BRIGHT_MAX)
-        show_brightness = BRIGHT_MAX;
-  
+      show_brightness = constrain(show_brightness, BRIGHT_MIN, BRIGHT_MAX);
+
       leds->setBrightness(show_brightness);
       leds->show();
-      Serial.print("+!#");
+      sendOk();
     } else if (key == 'm') {
       // set show MODE
       // m xxx
-      // set the mode here...
-      Serial.print("+!#");
+      uint8_t old_value = show_mode;
+      char show_value[3] = "";
+      show_value[0] = input_string[2];
+      show_value[1] = input_string[3];
+      show_value[2] = input_string[4];
+      show_value[3] = '\0';
+      show_mode = atoi(show_value);
+
+      if(show_mode > 2 || show_mode < 1) {
+        show_mode = old_value;
+      }
+
+      leds->setBrightness(show_brightness);
+      leds->show();
+      sendOk();
+    } else {
+      sendNOK(); // unknown key, not OK
     }
+  } else {
+    sendNOK(); // unknown type, not OK
   }
 
   resetInput();
 }
 
-// reset input variables
-void resetInput() {
-  input_state = LOW;
-  input_count = 0;
-  memset(input_string, 0, sizeof input_string);
-}
-
+// set up serial communication and LED(s)
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(38400);
   leds = new Adafruit_NeoPixel(NUMPIXELS, NEOPIN, NEOFORMAT);
   leds->begin();
   leds->clear();
   leds->setBrightness(show_brightness);
-  colorWipe(leds->Color(r, g, b));
+
+  current_color = leds->Color(0, 0, 0); // default color, 000 = black (off)
+  colorWipe(0, current_color);
+  wait_state = true;
 }
 
+// run the main loop (communication and state changes)
 void loop() {
   bool newState = LOW;
-  counter += 1;
 
   if (Serial.available() > 0) {
-    int input = Serial.read();
-
-    // DEBUG
-    // Serial.print("> ");
-    // Serial.println(input, DEC);
-
-    if (input_state == HIGH) {
-      if (input == 35) {  // #
-        // sequence complete
-        newState = HIGH;
+    // get serial input if available
+    newState = receiveInput();
+  } else if (wait_state) {
+    // wait for host connection
+    // fade LEDs while waiting
+    colorFade();
+  } else {
+    // set LEDs to proper state
+    if (show_mode == 2) {
+      if ((time % 2) == 0) {
+        leds->setBrightness(show_brightness);
       } else {
-        // receiving data
-        if (input_count > 10) {
-          input_state = LOW;
-          Serial.println("-!#");
-          resetInput(); 
-        } else {
-          input_string[input_count] = input;
-          input_count += 1;
-        }
+        leds->setBrightness(1);
       }
-    } else if ((input == 43 || input == 45) && input_state == LOW) {  // +
-      // receiving command/request
-      input_state = HIGH;
-      input_string[input_count] = input;
-      input_count += 1;
     }
+    colorWipe(0, current_color);
+
+    time++;
+
+    if (time > host_timeout) {
+      // it's past timeout! prepare for fade and send ping to host
+      time = 0;
+      wait_state = true;
+      show_mode = 1;
+      leds->setBrightness(50);
+      r = 0;
+      g = 64;
+      b = 0;
+      sendPing();
+    }
+    delay(1000);
   }
 
-  // handle input
   if (newState == HIGH) {
+    // handle serial input if any
     handleInput();
   }
 }
